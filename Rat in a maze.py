38693af26dc45fee78588f6dd11c9d04360358a0
cgaonkar04 <113157887+cgaonkar04@users.noreply.github.com>
2024-05-22 12:29:41 +0530

    Update Rat in a maze.py

--- Rat in a maze.py
+++ Rat in a maze.py
@@ -1,12 +1,15 @@
 grid = [[1, 0, 0, 0], [1, 1, 0, 1], [1, 1, 0, 0], [0, 1, 1, 1]]
 n = len(grid)
+ans = ""
 
 def is_valid(r, c):
     return 0 <= r < n and 0 <= c < n and grid[r][c] == 1
 
-def find_path(cur_r, cur_c, path):
+def path(cur_r, cur_c, seq):
+    global ans  # Use global keyword to modify the global variable 'ans'
     if cur_r == n - 1 and cur_c == n - 1:
-        return True, path
+        ans = seq
+        return True
     
     # Possible moves: down and right
     moves = [[cur_r + 1, cur_c], [cur_r, cur_c + 1]]
@@ -14,27 +17,24 @@ def find_path(cur_r, cur_c, path):
     for move in moves:
         new_r, new_c = move
         if is_valid(new_r, new_c):
-            # Mark as visited
-            grid[new_r][new_c] = 2  # Use 2 to mark visited cells
+            grid[new_r][new_c] = 2  # Mark as visited
             
             if move == [cur_r + 1, cur_c]:
-                new_path = path + "D"
+                new_seq = seq + "D"
             else:
-                new_path = path + "R"
+                new_seq = seq + "R"
             
-            found, result_path = find_path(new_r, new_c, new_path)
-            if found:
-                return True, result_path
+            if path(new_r, new_c, new_seq):
+                return True
             
             # Backtracking
             grid[new_r][new_c] = 1
     
-    return False, path
+    return False
 
 # Initial call to find the path
-found, result_path = find_path(0, 0, "")
-if found:
-    print(result_path)
+if path(0, 0, ""):
+    print(ans)
 else:
     print("No path found")
-print(grid)    
+
